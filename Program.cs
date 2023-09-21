2a905636c6b6ce25840bd2a53df92ae2bb6924c8
Princefaruqtella <princefaruqtella@gmail.com>
2023-09-21 03:00:49 -0700

    First Commit

---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿//Console.WriteLine("Welcome to MGQS: Input your gender");
+//string gender = Console.ReadLine(); 
+//gender = gender.ToLower();
+
+/*switch(gender)
+{
+    case "male":
+    Console.WriteLine("Asalamualykum Warahmotullah Wabarahkatuh You are a Male Student");
+    break;
+
+    case "female":
+    Console.WriteLine("Asalamualykum Warahmotullah Wabarahkatuh You are a Female Student");
+    break;
+
+    default:
+    Console.WriteLine("You are niether Male or Femaler");
+    break;
+}
+*/
+/*Random rand =new();
+int monthlySubscription = rand.Next(1, 6);
+string rollOver = "Kindly roll over and get a 10 % discount bonous, Thanks for choosing us";
+string subExpiry = "Your subscription will soon be over";
+string subEnd = "Your subscription will soon expiry tommorow";
+string greetings = "Thanks for choosing MTN";
+switch (monthlySubscription)
+{
+    case int subscription when monthlySubscription == 5:
+    Console.WriteLine($"Notification: {subExpiry}\n {rollOver}");
+    break;
+    case int subscription when monthlySubscription == 4:
+    Console.WriteLine($"Notification: {subExpiry}\n {rollOver}");
+    break;
+default:   
+    Console.WriteLine("Invalid operation entered");
+    break;
+}
+*/
+
+
+/*Random rand =new();
+//int monthlySubscription = rand.Next(1, 6);
+string rollover = "Kindly roll over and get a 10/100 discount bonous, Thanks for choosing us";
+string subExpiry = "Your subscription will soon be over";
+string subEnd = "Your subscription will soon expiry tommorow";
+string greetings = "Thanks for choosing MTN";
+if (monthlySubscription ==5)
+{
+    Console.WriteLine($"Notification: {subExpiry}\n {rollover}");
+}
+if (monthlySubscription <=4)
+{
+    Console.WriteLine($"Notification: {subExpiry}\n {greetings}");
+}
+*/
+
+/* Console.WriteLine("Calculate\n What opeation would you like to do");
+string operation = Console.ReadLine()!.ToLower();
+Console.WriteLine("Enter Two Numbers");
+Console.WriteLine("------------------------");
+Console.WriteLine("Enter First Numbers");
+int number1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter Second Numbers");
+int number2 = int.Parse(Console.ReadLine()!);
+int operationResult;
+switch(operation)
+{
+    case "+":
+    operationResult = number1 + number2;
+    Console.WriteLine($"The result of operation: {operationResult}");
+    break;
+    case "-":
+    operationResult = number1 - number2;
+    Console.WriteLine($"The result of operation: {operationResult}");
+    break;
+    case "*":
+    operationResult = number1 * number2;
+    Console.WriteLine($"The result of operation: {operationResult}");
+    break;
+    case "/":
+    operationResult = number1 / number2;
+    Console.WriteLine($"The result of operation: {operationResult}");
+    break;
+    default:
+    Console.WriteLine("Invalid operation entered");
+    break;
+}
+*/
+
